85089fdfebcb4d1d7a9506debf2744c81913243d
Lindanella <146224946+Lindanella@users.noreply.github.com>
2024-01-18 10:47:02 -0500

    my information v2

---
+++ my information v2.py
@@ -0,0 +1,7 @@
+#CS175L
+#LindaPimpinella
+#myInfo
+print('My name is: Linda')
+print ('My address is: Brooklyn, New York, 01234')
+print ('My number is: 123-456-7890')
+print ('My major is: Software engineering') 
